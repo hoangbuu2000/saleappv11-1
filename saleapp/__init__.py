c56615e9d2b608a2d8084b0f2162873535c945bd
hoangbuu2000 <49800665+hoangbuu2000@users.noreply.github.com>
2020-11-18 14:59:45 +0700

    thuc hanh buoi 5

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, AdminIndexView
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fdklajfiouiouqioekldsmkldamlk'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456789@localhost/saledbv1?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/saledbv1?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['ROOT_PROJECT_PATH'] = os.path.join(app.root_path, '/saleappv11/saleapp')
 
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name="IT81 SHOP", template_mode="bootstrap4")
--- saleapp/main.py
+++ saleapp/main.py
@@ -3,6 +3,7 @@ from saleapp import app, utils, models, login
 from saleapp.admin import *
 from flask_login import login_user
 import hashlib
+import os
 
 
 @app.route("/")
@@ -10,6 +11,37 @@ def index():
     categories = utils.read_data()
     return render_template('index.html',
                           categories=categories)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    err_msg = ''
+    if request.method == 'POST':
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password", '').strip()
+        confirm_password = request.form.get("confirm_password", '').strip()
+        email = request.form.get("email")
+
+        if password == confirm_password:
+            # import pdb
+            # pdb.set_trace() -> Debug
+
+            avatar = request.files['avatar']
+            avatar_path = 'images/upload/%s' % avatar.filename
+            path = os.path.join(app.root_path, 'static/', avatar_path)
+            avatar.save(path)
+
+            password = str(hashlib.md5(password.encode("utf8")).hexdigest())
+            u = models.User(name=name, username=username, password=password, email=email, avatar=avatar_path)
+            db.session.add(u)
+            db.session.commit()
+
+            return redirect('/')
+        else:
+            err_msg = "Mat khau khong khop"
+
+    return render_template('register.html', err_msg=err_msg)
 
 
 @app.route("/products")
@@ -45,8 +77,8 @@ def load_user(user_id):
 def login_admin():
     if request.method == 'POST':
         username = request.form.get("username")
-        password = request.form.get("password")
-        password = str(hashlib.md5(password.encode("utf8")).hexdigest())
+        password = request.form.get("password", "")
+        password = str(hashlib.md5(password.strip().encode("utf8")).hexdigest())
         user = models.User.query.filter(models.User.username == username, models.User.password == password).first()
 
         if user:
